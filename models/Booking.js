56395ba82380c5736e7fe1de2344e86b28558afa
mohammedrashed1 <mhmdmqdad112@gmail.com>
2026-04-26 18:12:24 +0300

    final code

--- models/Booking.js
+++ models/Booking.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 
 //Booking Schema
 const bookingSchema = new mongoose.Schema({
-    passengerName:{
-        type:String,
-        required:true
-    },
     flight:{
         type: mongoose.Schema.Types.ObjectId,
         ref:'Flight',
@@ -35,15 +31,7 @@ const bookingSchema = new mongoose.Schema({
         type:String,
         required:true,
         enum:['ChickenMeal','BeefMeal','VegetarianMeal','Snacks']
-    },
-    isRoundTrip:{
-        type:Boolean,
-        default: false
-    },
-    returnDate:{
-        type:Date
-    },
-
+    }
 },  
 {timestamps:true}   
 
